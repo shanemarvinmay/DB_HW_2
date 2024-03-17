87c2fa47d81f97a0d09c7e7c190a688431238847
shanemarvinmay <shanemarvinmay@gmail.com>
2024-03-17 13:13:27 -0700

    Clean acronym in data cleaning phase.

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,43 @@
 import pandas as pd
 
+def parse_conference_acronym(conference):
+    print(conference)
+    # Removing the year from the acronym.
+    conference_parts = conference.split(' ')
+    if conference_parts[-1].isnumeric():
+        conference_parts.pop()
+    conference = ''.join(conference_parts)
+    # Make sure the acronym is upper case and without whitespace.
+    return conference.strip().upper()
+
+def parse_conference_name(conference):
+    # Removing part with parathensis that contain the acronym and year.
+    parts = conference.split('(')
+    i = 0
+    while i < len(parts):
+        if ')' in parts[i]:
+            del parts[i]
+        else:
+            i += 1
+    conference = ''.join(parts)
+    # Removing any numeric parts like '12th' and year.
+    conference_parts = conference.split(' ')
+    i = 0
+    while i < len(conference_parts):
+        contains_number = any([char for char in conference_parts[i] if char.isdigit()])
+        if len(conference_parts[i]) < 1 or contains_number:
+            del conference_parts[i]
+        else:
+            i += 1
+    conference = ' '.join(conference_parts)
+    # Make sure the acronym is title case and without whitespace.
+    return conference.strip().title()
+
 if __name__ == '__main__':
-    # write about consider cases such as empty locations/online, repeatedly posted conferences, trim whitespace, etc.
+    '''
+    EXERCISE 2
+    '''
+    # df.columns are ['acronym', 'name', 'where']
     df = pd.read_csv('MAY_EXERCISE_1_OUTPUT.csv', sep='\t')
     # Removing duplicates
     df = df.drop_duplicates()
@@ -9,8 +45,6 @@ if __name__ == '__main__':
     # TODO write about fill in missign locations with online, since they all seem to be online journals.
     df['where'] = df['where'].fillna('Online')
     # TODO write about handling this missing city because it wasn't parsed right.
-    # where JÃ¶nkÃ¶ping ->  Jönköping
-    # df['where'] = df['where'].map({'JÃ¶nkÃ¶ping': 'Jönköping'})
     # Triming whitespace
     for column in df.columns:
         df[column] = df[column].str.strip()
@@ -18,56 +52,33 @@ if __name__ == '__main__':
         df[column] = df[column].str.replace('\r', '')
         df[column] = df[column].str.replace('\t', ' ')
         df[column] = df[column].str.replace('.', ',')
-        # replacing „
+        # Replacing '„' with ','. 
         df[column] = df[column].str.replace(chr(132), ',')
-        # replace ¶ with Ö
+        # Replacing '¶' with 'Ö'.
         df[column] = df[column].str.replace(chr(182), chr(214))
-    # Checking to make sure whitespace has been stripped.
+    
+    # Creating the year column.
+    years = []
     for idx, row in df.iterrows():
-        for col in df.columns:
-            print(f"|{row[col]}|", end='\t')
-        print()
-    # missing value stuff
-    # print(df.columns) ['acronym', 'name', 'where']
-    # where nan
-    for col in df.columns:
-        if df[col].isnull().values.any():
-            print(col)
-    for idx, row in df.iterrows():
-        # if pd.isna(row['where']) and 'ournal' not in row['name']:
-        #     print(row['acronym'], row['name'])  
-        if row['where'] == 'JÃ¶nkÃ¶ping':
-            for i in row['where']:
-                print(i, ord(i))
-            print(row['acronym'], row['name'])  
-        # if row['where'] == 'Mainz, Germany (Rheingoldhalle) & online':
-        #     for i in row['name']:
-        #         print(i, ord(i))
-    # duplicate stuff
-    # a = df['A'].unique()
-    # print(sorted(a))
-    # df.B.unique()
-    # boolean = not df["Student"].is_unique      # True (credit to @Carsten)
-    # boolean = df['Student'].duplicated().any() # True
-    # cols = list(df.columns)
-    # dups = [idx for idx, value in enumerate(df.duplicated()) if value]
-    # print(dups)    
-    # dups = [idx for idx, value in enumerate(df.duplicated()) if value]
-    # print(dups)
-    # for idx, row in df.iterrows():
-    #     if idx in dups:
-    #         print(row)
-    # for col in cols:
-    #     if df[col].is_unique:
-    #         continue
-    #     print(col)
-    #     dups = df[col].duplicated()
-    #     dup_ids = set()
-    #     for i in range(len(dups)):
-    #         if dups[i]:
-    #             dup_ids.add(i)
-    #     for id, row in df.iterrows():
-    #         if  id in dup_ids:
-    #             print(row[col])
+        try:
+            year = int(row['acronym'][-4:])
+            years.append(year)
+        except:
+            raise Exception(row['acronym'], row['name'], row['where'])
+    df['year'] = years
+    
+    # Cleaning up the acronym column
+    df['acronym'] = df.apply(lambda row: parse_conference_acronym(row['acronym']), axis=1)
+
+    # Saving the data.
     df[['acronym', 'name', 'where']].to_csv('MAY_EXERCISE_2_OUTPUT.csv', sep='\t', encoding='utf-8', index=False)
+
+    '''
+    CLEAN DATA FOR FUTURE EXERCISES
+    '''
+    
+
+    # Saving the data.
+    df.to_csv('MAY_conference_data.csv', sep='\t', encoding='utf-8', index=False)
+
     print("Done")
